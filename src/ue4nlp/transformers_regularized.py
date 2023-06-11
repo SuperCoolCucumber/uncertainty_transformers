f783dba52995ae119822a20434c2ca0010c56a4f
Daria <darianow112@gmail.com>
2023-06-11 18:14:11 +0300

    Add adapter trainers

--- src/ue4nlp/transformers_regularized.py
+++ src/ue4nlp/transformers_regularized.py
@@ -18,6 +18,8 @@ from transformers.modeling_outputs import (
     BaseModelOutputWithPoolingAndCrossAttentions,
 )
 from transformers import Trainer
+from transformers.adapters import AdapterTrainer
+
 
 from torch.autograd import Variable
 
@@ -334,6 +336,199 @@ class SelectiveTrainer(Trainer):
 
 
 class SelectiveSNGPTrainer(SelectiveTrainer, SNGPTrainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.reg_type = getattr(kwargs["args"], "reg_type", "reg-curr")
+        self.lamb = getattr(kwargs["args"], "lamb", 0.01)
+        self.margin = getattr(kwargs["args"], "margin", 0.5)
+        self.lamb_intra = getattr(kwargs["args"], "lamb_intra", 0.5)
+        self.unc_threshold = getattr(kwargs["args"], "unc_threshold", 0.5)
+
+
+
+
+class AdaptersSelectiveTrainer(AdapterTrainer):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.task = getattr(kwargs["args"], "task", "cls")
+        self.reg_type = getattr(kwargs["args"], "reg_type", "reg-curr")
+        self.lamb = getattr(kwargs["args"], "lamb", 0.01)
+        self.margin = getattr(kwargs["args"], "margin", 0.5)
+        self.lamb_intra = getattr(kwargs["args"], "lamb_intra", 0.5)
+        self.unc_threshold = getattr(kwargs["args"], "unc_threshold", 0.5)
+        if self.task == "cls":
+            self.unpad = False
+        else:
+            self.unpad = True
+
+    def post_init(self, reg_type, lamb, margin, lamb_intra, unc_threshold):
+        """Add regularization params"""
+        self.reg_type = reg_type
+        self.lamb = lamb
+        self.margin = margin
+        self.lamb_intra = lamb_intra
+        self.unc_threshold = unc_threshold
+
+    def compute_loss(self, model, inputs, return_outputs=False):
+        if type(model) is DistributedDataParallel:
+            model = model.module
+        labels = inputs.pop("labels")
+        output_hidden_states = True if self.reg_type == "metric" else False
+        outputs = model(**inputs, output_hidden_states=output_hidden_states)
+        logits = outputs.logits if self.task == "cls" else outputs[0]
+        if self.reg_type == "metric":
+            hiddens = outputs.hidden_states[-1][:, 0, :] if self.task == "cls" else outputs[1][-1]
+            if self.task == "cls":
+                del outputs
+                torch.cuda.empty_cache()
+                outputs = logits
+        if model.config.num_labels == 1:
+            #  We are doing regression
+            loss_fct = MSELoss()
+            loss = loss_fct(logits.view(-1), labels.view(-1))
+        else:
+            loss_fct = CrossEntropyLoss()
+            loss = loss_fct(logits.view(-1, model.config.num_labels), labels.view(-1))
+
+        if self.reg_type == "raw":
+            pass
+        elif self.reg_type == "reg-curr":
+            loss = compute_loss_cer(logits, labels, loss, self.lamb,
+                                    unpad=self.unpad)
+        elif self.reg_type == "metric":
+            loss = compute_loss_metric(hiddens, labels, loss,
+                                       model.config.num_labels,
+                                       self.margin, self.lamb_intra, self.lamb,
+                                       unpad=self.unpad)
+            if self.task == "ner":
+                # we don't need hiddens anymore
+                outputs = outputs[0]
+        elif self.reg_type == "rau":
+            loss += self.lamb * RAU_loss(torch.softmax(logits, dim=1), labels, self.unc_threshold)
+        else:
+            raise NotImplementedError()
+        if isinstance(outputs, tuple):
+            return (loss,) + outputs if return_outputs else loss
+        else:
+            return (loss, outputs) if return_outputs else loss
+
+    def prediction_step(
+        self,
+        model: nn.Module,
+        inputs: Dict[str, Union[torch.Tensor, Any]],
+        prediction_loss_only: bool,
+        ignore_keys: Optional[List[str]] = None,
+    ) -> Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]:
+        """
+        Perform an evaluation step on :obj:`model` using obj:`inputs`.
+
+        Subclass and override to inject custom behavior.
+
+        Args:
+            model (:obj:`nn.Module`):
+                The model to evaluate.
+            inputs (:obj:`Dict[str, Union[torch.Tensor, Any]]`):
+                The inputs and targets of the model.
+
+                The dictionary will be unpacked before being fed to the model. Most models expect the targets under the
+                argument :obj:`labels`. Check your model's documentation for all accepted arguments.
+            prediction_loss_only (:obj:`bool`):
+                Whether or not to return the loss only.
+            ignore_keys (:obj:`Lst[str]`, `optional`):
+                A list of keys in the output of your model (if it is a dictionary) that should be ignored when
+                gathering predictions.
+
+        Return:
+            Tuple[Optional[torch.Tensor], Optional[torch.Tensor], Optional[torch.Tensor]]: A tuple with the loss,
+            logits and labels (each being optional).
+        """
+        has_labels = all(inputs.get(k) is not None for k in self.label_names)
+        inputs = self._prepare_inputs(inputs)
+        if ignore_keys is None:
+            if hasattr(self.model, "config"):
+                ignore_keys = getattr(
+                    self.model.config, "keys_to_ignore_at_inference", []
+                )
+            else:
+                ignore_keys = []
+
+        # labels may be popped when computing the loss (label smoothing for instance) so we grab them first.
+        if has_labels:
+            labels = nested_detach(tuple(inputs.get(name) for name in self.label_names))
+            if len(labels) == 1:
+                labels = labels[0]
+        else:
+            labels = None
+
+        with torch.no_grad():
+            if is_sagemaker_mp_enabled():
+                raw_outputs = smp_forward_only(model, inputs)
+                if has_labels:
+                    if isinstance(raw_outputs, dict):
+                        loss_mb = raw_outputs["loss"]
+                        logits_mb = tuple(
+                            v
+                            for k, v in raw_outputs.items()
+                            if k not in ignore_keys + ["loss"]
+                        )
+                    else:
+                        loss_mb = raw_outputs[0]
+                        logits_mb = raw_outputs[1:]
+
+                    loss = loss_mb.reduce_mean().detach().cpu()
+                    logits = smp_nested_concat(logits_mb)
+                else:
+                    loss = None
+                    if isinstance(raw_outputs, dict):
+                        logits_mb = tuple(
+                            v for k, v in raw_outputs.items() if k not in ignore_keys
+                        )
+                    else:
+                        logits_mb = raw_outputs
+                    logits = smp_nested_concat(logits_mb)
+            else:
+                if has_labels:
+                    loss, outputs = self.compute_loss(
+                        model, inputs, return_outputs=True
+                    )
+                    loss = loss.mean().detach()
+                    if isinstance(outputs, dict):
+                        logits = tuple(
+                            v
+                            for k, v in outputs.items()
+                            if k not in ignore_keys + ["loss"]
+                        )
+                    else:
+                        # Changed from original code - there was outputs[1:] for some reason
+                        logits = outputs
+                else:
+                    loss = None
+                    if self.use_amp:
+                        with autocast():
+                            outputs = model(**inputs)
+                    else:
+                        outputs = model(**inputs)
+                    if isinstance(outputs, dict):
+                        logits = tuple(
+                            v for k, v in outputs.items() if k not in ignore_keys
+                        )
+                    else:
+                        logits = outputs
+                    # TODO: this needs to be fixed and made cleaner later.
+                    if self.args.past_index >= 0:
+                        self._past = outputs[self.args.past_index - 1]
+
+        if prediction_loss_only:
+            return (loss, None, None)
+
+        logits = nested_detach(logits)
+        if len(logits) == 1:
+            logits = logits[0]
+
+        return (loss, logits, labels)
+
+
+class AdaptersSelectiveSNGPTrainer(AdaptersSelectiveTrainer, SNGPTrainer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.reg_type = getattr(kwargs["args"], "reg_type", "reg-curr")
